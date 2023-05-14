8e27f8794189566229495161772c4c0c7c005ea9
dehwyy <91pony@mail.ru>
2023-05-14 05:22:15 +0300

    profile page basement

---
+++ src/app/profile/components/ExpandingBlock.tsx
@@ -0,0 +1,27 @@
+import { ChevronDownIcon, ChevronUpIcon } from '@radix-ui/react-icons'
+import { useState } from 'react'
+
+interface IProps {
+  children: React.ReactNode
+}
+
+const ExpandingBlock = ({ children }: IProps) => {
+  const [isOpened, setIsOpened] = useState(true)
+  return (
+    <div>
+      <div className="flex select-none bg-blue-700 w-full p-5 rounded-lg ">
+        <h3 className="text-2xl">Used layouts</h3>
+        <span>
+          {isOpened ? (
+            <ChevronUpIcon onClick={() => setIsOpened(false)} className="h-full w-5 cursor-pointer" />
+          ) : (
+            <ChevronDownIcon onClick={() => setIsOpened(true)} className="h-full w-5 cursor-pointer" />
+          )}
+        </span>
+      </div>
+      <div className={`${isOpened ? 'top-20 visible opacity-100' : 'top-12 invisible opacity-0'} w-full absolute transition-all `}>{children}</div>
+    </div>
+  )
+}
+
+export default ExpandingBlock
--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,13 +1,64 @@
-interface IProps {
+'use client'
+import { Roboto } from 'next/font/google'
+import { useState } from 'react'
+import { ChevronDownIcon, ChevronUpIcon } from '@radix-ui/react-icons'
+import ExpandingBlock from '@/app/profile/components/ExpandingBlock'
 
-}
+interface IProps {}
+
+const ExtraboldText = Roboto({ subsets: ['latin'], weight: '900' })
+
+const layouts = [
+  {
+    id: 1,
+    tasksCount: 5,
+    at: new Date(1684030022488),
+    allTasks: [
+      { id: 1, name: 'Play starrail' },
+      { id: 2, name: 'Omatsuri ni iku' },
+    ],
+  },
+  {
+    id: 2,
+    tasksCount: 3,
+    at: new Date(1684030022488),
+    allTasks: [
+      { id: 1, name: 'Watch mrekk' },
+      { id: 2, name: 'Drink water' },
+    ],
+  },
+]
 
 const Page = ({}: IProps) => {
-    return (
-        <div>
-            <div></div>
-        </div>
-    )
+  const nickname = 'dehwyy'
+  return (
+    <div className="max-w-[1000px] mx-auto py-12 flex flex-col w-full gap-5">
+      <section>
+        <h2 className="text-3xl text-gray-200 text-center">
+          Signed as <span className={`${ExtraboldText.className} text-white underline-offset-4 underline decoration-2`}>{nickname}</span>
+        </h2>
+      </section>
+      <section className="w-full relative">
+        <ExpandingBlock>
+          {layouts.map(layout => (
+            <div key={layout.id} className="first:rounded-t-lg last:rounded-b-lg bg-blue-600 py-7 px-2  grid grid-cols-3 w-full">
+              <div className="text-center">
+                At {layout.at.toLocaleTimeString()} {String(layout.at.getDate()).padStart(2, '0')}.{String(layout.at.getMonth()).padStart(2, '0')}.
+                {String(layout.at.getFullYear()).slice(2)}
+              </div>
+              <div className="text-center">Total {layout.tasksCount} tasks</div>
+              <div className="flex flex-col">
+                <span className="underline decoration-2">Tasks:</span>
+                {layout.allTasks.map(task => (
+                  <div key={task.id}>{task.name}</div>
+                ))}
+              </div>
+            </div>
+          ))}
+        </ExpandingBlock>
+      </section>
+    </div>
+  )
 }
 
 export default Page
