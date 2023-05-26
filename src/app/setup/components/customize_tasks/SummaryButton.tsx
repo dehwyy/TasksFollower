771818d43c00210f0185fe39a835a66735ae4673
dehwyy <91pony@mail.ru>
2023-05-26 03:50:46 +0300

    add styles on summaryButton

--- src/app/setup/components/customize_tasks/SummaryButton.tsx
+++ src/app/setup/components/customize_tasks/SummaryButton.tsx
@@ -1,12 +1,16 @@
 import { useAtom } from 'jotai/index'
 import { globalModalWindowsAtom } from '@/utlis/store/globalModal'
+import isAbleToWatchSummaryModal from '@/utlis/hooks/customize_hooks/isAbleToWatchSummaryModal'
 
 const SummaryButton = () => {
   const [_, setGlobalModalWindow] = useAtom(globalModalWindowsAtom)
+  const { isAbleToWatch } = isAbleToWatchSummaryModal()
   return (
     <div
       onClick={() => setGlobalModalWindow({ uid: 'summaryWindow', state: true })}
-      className="cursor-pointer rounded-xl text-pinkLight border-4 border-violetBorderPale text-2xl py-4 mt-5 px-5 flex w-full justify-center">
+      className={`${
+        isAbleToWatch ? 'cursor-pointer hover:scale-105 hover:border-violetBorderLighter' : 'text-opacity-50'
+      } rounded-xl text-pinkLight border-4  border-violetBorderPale text-2xl py-4 mt-5 px-5 flex w-full justify-center duration-300 transition-all`}>
       Summary
     </div>
   )
