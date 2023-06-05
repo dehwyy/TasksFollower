3c87fde048d06f58a52963daa70abd3f304a0c8e
dehwyy <dehwyy@yandex.ru>
2023-06-05 04:36:06 +0300

    feat: remake appearance of useTaskPage

--- src/app/components_for_index_page/task/Component.tsx
+++ src/app/components_for_index_page/task/Component.tsx
@@ -4,10 +4,9 @@ import TaskButtons from '@/app/components_for_index_page/task/TaskButtons'
 const Component = () => {
   return (
     <div className="w-full pb-10">
-      <div className="flex flex-col w-1/4 mx-auto py-10">
+      <div className="flex flex-col w-1/2 mx-auto py-10">
         <TaskJobs />
       </div>
-      <TaskButtons />
     </div>
   )
 }
--- src/app/components_for_index_page/task/TaskJobs.tsx
+++ src/app/components_for_index_page/task/TaskJobs.tsx
@@ -1,42 +1,26 @@
-'use client'
-import { useAtomValue } from 'jotai'
-import { TaskPlayData } from '@/utlis/store/task/task.play'
-import useJobs from '@/utlis/hooks/play_hooks/useJobs'
-import useAllDataFromStore from '@/utlis/hooks/play_hooks/useAllDataFromStore'
+interface IProps {}
 
 const TaskJobs = () => {
-  const { isValid } = useAllDataFromStore()
-  const { value: jobs } = useAtomValue(TaskPlayData.jobs)
-  const { totalProgress, getRestProgress } = useJobs()
-  return isValid ? (
-    <>
-      <div className="relative">
-        <div style={{ height: 'calc(100% - 45px)' }} className="absolute bg-pinkLight min-w-[4px] left-[49%] right-[49%] top-[45px] ">
-          <div
-            style={{ top: totalProgress }}
-            className="absolute bg-gray-400 z-20 left-0 right-0 bottom-0 transition-all duration-1000 ease-linear"
-          />
+  return (
+    <div className="w-full flex flex-col">
+      {['1', '2'].map(job => (
+        <div key={job} className="w-full text-center">
+          <div className="w-full flex flex-col items-center">
+            {/* ! if isPassed => shadow-lg else shadow-0*/}
+            <div className="select-none border-2 bg-violetLightPaler border-violetLightBorder shadow-lg shadow-violetLight w-7/12 py-2 rounded-lg">
+              {job}
+            </div>
+            <div className="h-[170px] w-[5px] bg-gray-500" />
+          </div>
+          <div className="w-full flex flex-col items-center">
+            <div className="select-none border-2 bg-violetPale border-violetBorderLighter shadow-lg shadow-violetBorderLighter w-1/2 py-2 rounded-lg">
+              Rest
+            </div>
+            <div className="h-[80px] w-[5px] bg-gray-500" />
+          </div>
         </div>
-        <div className="flex flex-col gap-y-20">
-          {/*It definitely would be Array, ArrayIsArray usage just for Typescript*/}
-          {Array.isArray(jobs) &&
-            jobs.map((job, i) => (
-              <div className="w-full overflow-hidden">
-                <div className="w-full relative bg-gray-400 h-[45px] rounded-xl overflow-hidden">
-                  <div
-                    style={{ bottom: getRestProgress(i) }}
-                    className="absolute h-full bg-pinkLight left-0 right-0 bottom-[10%] z-20 transition-all duration-1000 ease-linear"></div>
-                  <div className="absolute bg-backgroundColor top-[2px] left-[2px] right-[2px] bottom-[2px] z-30 rounded-xl">
-                    <div className="w-full h-full block-item flex items-center justify-center">{job}</div>
-                  </div>
-                </div>
-              </div>
-            ))}
-        </div>
-      </div>
-    </>
-  ) : (
-    <></>
+      ))}
+    </div>
   )
 }
 
